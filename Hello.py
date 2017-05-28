dc72ba6a4f5f40d04cc568e40d647199beac20ff
magdalena <magdalenadomagala@me.com>
2017-05-28 09:50:15 +0200

    My Django Girls app, first commit

---
+++ Hello.py
@@ -0,0 +1,15 @@
+"""def hej():
+	print ('Magda')
+hej()
+"""
+
+"""def hej(imie):
+    print('Hej ' + imie + '!')
+
+dziewczyny = ['Dominika', 'Anna', 'Grażyna', 'Basia', 'Dorota']
+for imie in dziewczyny:
+    hej(imie)
+"""
+
+for i in range (-25,15):
+	print (i)
